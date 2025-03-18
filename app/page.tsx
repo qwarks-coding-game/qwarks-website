36c7c484b783f54d3d6606d7dea274ea4d76fa5c
MatthewWHuang <thematthewhuang@gmail.com>
2025-03-18 18:01:10 -0400

    Added background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from 'next/image';
+import background from "./images/background.gif";
 
 export default function Home() {
   return (
     <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <div style={{ position: 'fixed', width: '100vw', height: '100vh', overflow: 'hidden', top: 0, zIndex: -1}}>
+        <Image src={background} alt={"An example QWARKS game"}
+        layout="fill"
+        objectFit="cover"/>
+      </div>
       <h1>Strategize. Code. Win.</h1>
       <div style={{width: "50vw"}}>
         <p>Code your AI bot in Python to control your army of QWARKS to take over energy sources and eliminate your enemies!</p>
