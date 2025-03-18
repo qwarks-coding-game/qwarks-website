5c28068d72f0be25b0f8dbc55a6e84ae3483abbf
MatthewWHuang <thematthewhuang@gmail.com>
2025-03-18 18:28:10 -0400

    Fixed installation page

--- app/install/page.tsx
+++ app/install/page.tsx
@@ -4,7 +4,8 @@ export default function About() {
       <h1>
         Installing QWARKS
       </h1>
-      <p>Installation instructions can be found <a href="https://docs.google.com/document/d/1ANnzO-5xZ3_dIKv6GeW00R77ga6XE5fO8304MbqZJF0/edit?usp=sharing">here</a>.</p>
+      <p>We're working on transferring the installation over to the website, but for now, you can view instructions using the link below!</p>
+      <button><a href="https://docs.google.com/document/d/1ANnzO-5xZ3_dIKv6GeW00R77ga6XE5fO8304MbqZJF0/edit?usp=sharing">Installation Instructions</a></button>
     </div>
   );
 }
